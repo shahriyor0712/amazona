56fb2e0b6b1763afd6961517d477634f13b0bae3
shahriyor0712 <xabibullayevshahriyor294@gmail.com>
2022-01-13 19:13:53 +0500

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:id" element={<ProductScreen />}></Route>
+            <Route path="/" element={<HomeScreen />} exact></Route>
+          </Routes>
+        </main>
+        <footer>All right reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function MessageBox(props) {
+    return (
+        <div>
+           
+        </div>
+    );
+}
